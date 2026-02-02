fed2ba93f8fca725d1f2c06592aaf721556d6bed
pdsyy <prodseoy@gmail.com>
2026-02-02 18:44:18 +0200

    resend key

--- api/send-email.js
+++ api/send-email.js
@@ -1,6 +1,6 @@
 import { Resend } from 'resend';
 
-const resend = new Resend("re_QsnDefeS_GUrc3kJ9t182MegeYd8nXdtA");
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export default async function handler(req, res) {
     // Разрешаем только POST запросы
